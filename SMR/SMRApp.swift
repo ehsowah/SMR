4d6f8d8dcd8e58271376c0bed63f8714f4e5b377
ehsowah <56480539+ehsowah@users.noreply.github.com>
2023-09-20 18:21:15 -0400

    Initial Commit

---
+++ SMR/SMRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMRApp.swift
+//  SMR
+//
+//  Created by ehsowah on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SMRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

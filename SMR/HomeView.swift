9983ac0a49197f93169eed5dfb293682e38f0403
ehsowah <56480539+ehsowah@users.noreply.github.com>
2023-09-22 10:48:50 -0400

    adding home view

--- SMR/HomeView.swift
+++ SMR/HomeView.swift
@@ -9,50 +9,65 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                            .fill(.red)
-                            .shadow(color: .black, radius: 20, x: 0, y: 10)
+        VStack {
             
-            VStack {
-                HStack(spacing: 20) {
-                    Image("logo")
-                        .resizable()
-                        .frame(width: 150)
-                        .aspectRatio(contentMode: .fit)
+            ZStack {
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .fill(.white)
+                                .shadow(color: .black, radius: 20, x: 0, y: 10)
+                
+                VStack {
+                    HStack(spacing: 20) {
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 150)
+                            .aspectRatio(contentMode: .fit)
+                            
                         
-                    
-                    VStack(alignment: .leading) {
-                        Text("Status")
-                            .font(.title3)
-                            .bold()
-                        HStack {
-                            Circle()
-                                .frame(width: 13)
-                                .foregroundColor(.green)
-                            Text("Connected")
-                        }
-                        
-                        Text("Power")
-                            .font(.title3)
-                            .bold()
-                        HStack {
-                            Circle()
-                                .frame(width: 13)
-                                .foregroundColor(.green)
-                            Text("100%")
+                        VStack(alignment: .leading) {
+                            Text("Status")
+                                .font(.title3)
+                                .bold()
+                            HStack {
+                                Circle()
+                                    .frame(width: 13)
+                                    .foregroundColor(.green)
+                                Text("Connected")
+                            }
+                            
+                            Text("Power")
+                                .font(.title3)
+                                .bold()
+                            HStack {
+                                Circle()
+                                    .frame(width: 13)
+                                    .foregroundColor(.green)
+                                Text("100%")
+                            }
                         }
                     }
+                    
+                    
+
                 }
+                .padding(30)
+                .cornerRadius(20)
             }
-            .padding(30)
-            .cornerRadius(20)
+            .frame(height: 250)
+            .padding()
         }
-        .frame(height: 250)
-        .padding()
         
-        
-        
+        Button {
+            print("action")
+        } label: {
+            Text("Camera")
+                .foregroundColor(.white)
+                .frame(width: 150)
+                .padding()
+                .background(Color.orange)
+                .cornerRadius(25)
+        }
+
     }
     
     

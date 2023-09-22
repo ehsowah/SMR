ecd9709d33f9d11a665d0cfe9027fce3209c5cb8
ehsowah <56480539+ehsowah@users.noreply.github.com>
2023-09-22 11:14:05 -0400

    adding one signal

--- SMR/SMRApp.swift
+++ SMR/SMRApp.swift
@@ -6,12 +6,37 @@
 //
 
 import SwiftUI
+import OneSignalFramework
 
 @main
 struct SMRApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Remove this method to stop OneSignal Debugging
+      OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+       
+      // OneSignal initialization
+      OneSignal.initialize("7d4304fb-d434-40df-a52d-c00590b112e0", withLaunchOptions: launchOptions)
+
+      // requestPermission will show the native iOS notification permission prompt.
+      // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+      OneSignal.Notifications.requestPermission({ accepted in
+        print("User accepted notifications: \(accepted)")
+      }, fallbackToSettings: true)
+
+      // Login your customer with externalId
+      // OneSignal.login("EXTERNAL_ID")
+           
+      
+        return true
     }
 }
